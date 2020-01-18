f9cdd925da49d05fae8ae05987effe4651d2cd1d
Novizar <novizarhadisaputra@gmail.com>
2020-01-18 08:28:44 +0700

    Bug Fixing

--- routes/transactions.js
+++ routes/transactions.js
@@ -20,7 +20,7 @@ router.post('/add', function (req, res, next) {
       res.render('transactions/add', {
         title: 'Add Transactions Page',
         error: true,
-        messages: 'Not already out'
+        messages: 'Exit already out'
       });
     } else {
       con.query(sql2, function (err2) {
@@ -50,14 +50,14 @@ router.get('/out/:id/:id_transactions', function (req, res, next) {
           res.redirect('/');
         });
       } else {
-        var sql2 = `SELECT * FROM transaksi WHERE id = ${req.params.id_transactions} ORDER BY masuk LIMIT 1`;
+        var sql2 = `SELECT  id, platnomor, DATE_FORMAT(masuk, "%Y-%m-%d") As masuk, keluar, total FROM transaksi WHERE id = ${req.params.id_transactions} ORDER BY masuk LIMIT 1`;
         con.query(sql2, function (err2, result2) {
           if (err2) throw err2;
-          var checkin = new Date(result2[0].mulai).getTime();
+          var checkin = new Date(result2[0].masuk).getTime();
           var checkout = Date.now();
-          var hours = (checkout - checkin) / 3600;
+         var hours = (checkout - checkin) / 3600;
           var total = 3000 * (hours > 0 && hours < 1 ? 1 : Math.round(hours));
-          var sql3 = `UPDATE transaksi SET total = ${total}  WHERE id = ${req.params.id_transactions}`;
+          var sql3 = `UPDATE transaksi SET total = ${total} AND SET checkout  WHERE id = ${req.params.id_transactions}`;
           con.query(sql3, function (err3) {
             if (err3) throw err3;
             res.redirect('/');
